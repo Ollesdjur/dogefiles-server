b3a6d574c4daab486e8030580b75727086a30158
iamrobins <itsrobin4@gmail.com>
2021-06-04 00:49:33 +0530

    comments removed

--- Controllers/S3.js
+++ Controllers/S3.js
@@ -179,46 +179,6 @@ export const objectInfo = async (req, res) => {
   }
 };
 
-// export const downloadUrl = async (req, res) => {
-//   const s3 = new AWS.S3({
-//     correctClockSkew: true,
-//     endpoint: process.env.AWS_S3_ENDPOINT,
-//     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-//     region: process.env.AWS_REGION,
-//     logger: console,
-//   });
-
-//   const { key } = req.query;
-
-//   try {
-//     const file = await File.findOne({ key });
-
-//     if (!file)
-//       return res.status(404).json({ error: "Invalid Key, Not file found" });
-
-//     if (file.privacy === "private")
-//       return res
-//         .status(400)
-//         .json({ error: "Private files are not availabe to download" });
-
-//     const params = {
-//       Bucket: "meow0007",
-//       Key: key,
-//       Expires: 1000,
-//       ResponseContentDisposition: `attachment; filename="${file.fileName}"`,
-//     };
-
-//     const url = s3.getSignedUrl("getObject", params);
-
-//     file.downloads += 1;
-//     await file.save();
-
-//     return res.status(200).json({ downloadLink: url });
-//   } catch (err) {
-//     res.status(404).json(err.message);
-//   }
-// };
 export const downloadUrl = async (req, res) => {
   const s3 = new AWS.S3({
     correctClockSkew: true,
@@ -228,7 +188,7 @@ export const downloadUrl = async (req, res) => {
     region: process.env.AWS_REGION,
     logger: console,
   });
-  // qKIJwQUnREePR9Yu7jisyTFVcJu1
+
   const { id } = req.query;
 
   try {
@@ -250,9 +210,6 @@ export const downloadUrl = async (req, res) => {
     };
 
     const url = s3.getSignedUrl("getObject", params);
-
-    // file.downloads += 1;
-    // await file.save();
 
     return res.status(200).json({ downloadLink: url });
   } catch (err) {
