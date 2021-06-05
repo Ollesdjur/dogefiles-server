975fa036a56a9ab2a24068318a8ea52871e95da6
iamrobins <itsrobin4@gmail.com>
2021-06-05 18:14:01 +0530

    sniff data

--- Routes/test.js
+++ Routes/test.js
@@ -1,21 +1,19 @@
 import express from "express";
 const router = express.Router();
 import IP_Logs from "../Models/IP_Logs.js";
+import sniffData from "../Middlewares/sniffData.js";
 
 const test = async (req, res, next) => {
   // const ipLog = await IP_Logs.create({});
   // console.log(ipLog);
-  console.log(req.ip);
-  console.log(req.connection.remoteAddress);
-  res
-    .status(200)
-    .json({
-      success: "You reached the route successfully",
-      ip: req.ip,
-      remoteA: req.connection.remoteAddress,
-    });
+  res.status(200).json({
+    success: "Your sniffed data",
+    ip: req.ip,
+    remoteA: req.connection.remoteAddress,
+    sniff_data: req.sniff_data,
+  });
 };
 
-router.route("/").get(test);
+router.route("/").get(sniffData, test);
 
 export default router;
