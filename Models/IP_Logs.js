e81109201c924fcfd1d543ad53f5c8444fe16759
iamrobins <itsrobin4@gmail.com>
2021-06-21 18:18:06 +0530

    updated ip logs deletion

--- Models/IP_Logs.js
+++ Models/IP_Logs.js
@@ -2,13 +2,15 @@ import mongoose from "mongoose";
 
 const IP_LogsSchema = new mongoose.Schema(
   {
-    createdAt: { type: Date, expires: 60, default: Date.now },
+    // createdAt: { type: Date, expires: 60, default: Date.now },
     ip: { type: String, required: true },
   },
   {
     timestamps: true,
   }
 );
+
+IP_LogsSchema.index({ createdAt: 1 }, { expireAfterSeconds: 60 });
 
 const IP_Logs = mongoose.model("IP_Logs", IP_LogsSchema);
 export default IP_Logs;
