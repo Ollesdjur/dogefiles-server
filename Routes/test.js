1c41a6aaacd999c8b7134715213fed6032604a0b
iamrobins <itsrobin4@gmail.com>
2021-06-21 19:16:56 +0530

    added file download logs 1 min time

--- Routes/test.js
+++ Routes/test.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import IP_Logs from "../Models/IP_Logs.js";
+import fileDownloadLogs from "../Models/fileDownloadLogs.js";
 import sniffData from "../Middlewares/sniffData.js";
 
 const test = async (req, res, next) => {
@@ -8,13 +8,13 @@ const test = async (req, res, next) => {
     req.sniff_data.ip_address.ip || req.sniff_data.ip_address.xForwardedFor;
   console.log(ip);
 
-  if (await IP_Logs.findOne({ ip: ip })) {
+  if (await fileDownloadLogs.findOne({ ip: ip })) {
     return res
       .status(200)
       .json({ err: "You can not download this file currently" });
   }
 
-  const ipLog = await IP_Logs.create({ ip: ip });
+  const ipLog = await fileDownloadLogs.create({ ip: ip });
 
   res.status(200).json({
     success: "Your sniffed data",
